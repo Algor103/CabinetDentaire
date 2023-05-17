009ef6009bf1353cf13dc9f25cd38a6012b301ad
Algor103 <junioralgor@gmail.com>
2023-05-17 05:04:59 +0200

    mise en place

---
+++ src/main/java/com/mycompany/cabinetdentaire/CabinetDentaire.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cabinetdentaire;
+
+/**
+ *
+ * @author ALGOR FOKA
+ */
+public class CabinetDentaire {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
